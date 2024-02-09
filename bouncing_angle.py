bfbffb74559fe3c2db1d0629f3e1846d0d91558c
Kapusbenjamin <47866619+Kapusbenjamin@users.noreply.github.com>
2024-02-09 19:57:59 +0100

    Falról pattanás javítása
    Határ átugrása.
    1 fel - 1 le lépegetés.
    Magasság állítása (first y értéke)

--- bouncing_angle.py
+++ bouncing_angle.py
@@ -1,34 +1,37 @@
 import random
 
 
-MAX_X = 250
-MAX_Y = 800
+max_x = 250
+max_y = 800
 
 starting_xy = [0,0]
 
 for _ in range(10):
 
-    first_xy = [random.randint(-250, 250), random.randint(0, 800)]
+    first_xy = [random.randint(-250, 250), random.randint(1, 20)]
 
     vector = [first_xy[0]-starting_xy[0], first_xy[1]-starting_xy[1]]
 
+    if(-max_x > vector[0]) or (vector[0] > max_x):
+        continue
+    
     x_add = 1
     y_add = 1
-
-    if vector[0] >= 0 : x_add = 1 
-    else: x_add = -1
+    if vector[0] >= 0:
+        x_add = 1
+    else:
+        x_add = -1
 
     while vector[1] > 0:
-        if vector[0] == MAX_X or vector[0] == -MAX_X:
-            x_add = -x_add
-            print(vector)
-            print(x_add)
-        if vector[1] == MAX_Y or vector[1] == -MAX_Y:
-            y_add = -y_add
-            print(vector)
-            print(y_add)
 
-        vector = [vector[0]+x_add, vector[1]+y_add]
+        if vector[0] == max_x or vector[0] == -max_x:
+            x_add = -x_add
+
+        if vector[1] == max_y:
+            y_add = -y_add
+
+        vector = [vector[0] + x_add, vector[1] + y_add]
 
     starting_xy = vector
-    print(starting_xy)
+    
+    
